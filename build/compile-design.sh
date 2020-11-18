c16cce357b70b1a0e2bbc7e37886cf3f8b2848eb
artlabs <74618151+artlabs@users.noreply.github.com>
2020-11-18 18:36:33 +0100

    Update compile-design.sh

--- build/compile-design.sh
+++ build/compile-design.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-SOURCE="../design/source"
+SOURCE="../design/source/w3bkit.scss"
 TARGET="../design/css"
+NAME="w3b"
 
-sass $SOURCE/w3bkit.scss $TARGET/w3b.css
-sass --style=compressed $SOURCE/w3bkit.scss $TARGET/w3b.min.css
+printf "compiling $NAME.css...\n"
+sass $SOURCE $TARGET/$NAME.css
+
+printf "compiling $NAME.min.css...\n"
+sass --style=compressed $SOURCE $TARGET/$NAME.min.css
 
